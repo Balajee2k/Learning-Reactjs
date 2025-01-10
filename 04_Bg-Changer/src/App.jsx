e3f7e85e0719f7e569d4b1e7c8532b7623751919
Balajee <jeebalajee7@gmail.com>
2025-01-10 22:26:20 +0530

    make a background changer project

---
+++ 04_Bg-Changer/src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+
+function App() {
+  const [color,setcolor]=useState("olive")
+
+  return (
+    <>
+      <div className="w-full h-screen duration-200" 
+      style={{backgroundColor: color}}>
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-2xl">
+          <button
+           onClick={()=>setcolor("red")}
+           className="outline-none px-4 rounded-full text-white shadow-lg"
+          style={{backgroundColor:"red"}}
+          >Red</button>
+
+          <button className="outline-none px-4 rounded-full text-white shadow-lg"
+          onClick={()=>setcolor("green")}
+          style={{backgroundColor:"green"}}
+          >Green
+          </button>
+
+          <button className="outline-none px-4 rounded-full text-white shadow-lg"
+          onClick={()=>setcolor("black")}
+          style={{backgroundColor:"black"}}
+          >Black
+          </button>
+
+          <button className="outline-none px-4 rounded-full text-white shadow-lg"
+          onClick={()=>setcolor("blue")}
+          style={{backgroundColor:"blue"}}
+          >Blue
+          </button>
+
+          <button className="outline-none px-4 rounded-full text-white shadow-lg"
+          onClick={()=>setcolor("pink")}
+          style={{backgroundColor:"pink"}}
+          >Pink
+          </button>
+
+          <button className="outline-none px-4 rounded-full text-white shadow-lg"
+          onClick={()=>setcolor("white")}
+          style={{backgroundColor:"black"}}
+          >White
+          </button>
+          
+
+        </div>
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
